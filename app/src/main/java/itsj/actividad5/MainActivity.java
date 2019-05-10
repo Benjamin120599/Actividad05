b541a2ae812d9e9a8257399a6a4d63b26a4df10a
Benjamin Viramontes <benja120599@gmail.com>
2019-05-10 14:54:25 -0500

    Se instanciaron las variables

--- app/src/main/java/itsj/actividad5/MainActivity.java
+++ app/src/main/java/itsj/actividad5/MainActivity.java
@@ -14,4 +14,23 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     EditText entrada, salida;
     Spinner spinner1, spinner2;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adaptador = ArrayAdapter.createFromResource(this, R.array.temperaturas, android.R.layout.simple_spinner_item);
+
+        entrada = findViewById(R.id.entrada);
+        salida = findViewById(R.id.salida);
+
+        spinner1 = findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(this);
+        spinner1.setAdapter(adaptador);
+        spinner2 = findViewById(R.id.spinner2);
+        spinner2.setOnItemSelectedListener(this);
+        spinner2.setAdapter(adaptador);
+
+    }
+
 }
