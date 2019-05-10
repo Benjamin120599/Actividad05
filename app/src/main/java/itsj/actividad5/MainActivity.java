1e0051c54c6fffc3e61250755577e5418eb5e382
Benjamin Viramontes <benja120599@gmail.com>
2019-05-10 14:51:37 -0500

    Se añadió el proyecto al repositorio

---
+++ app/src/main/java/itsj/actividad5/MainActivity.java
@@ -0,0 +1,16 @@
+package itsj.actividad5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+
+
+}
